11e06e4f686c5acd9ff88a5e7fcd067d1cacd039
cickib <bellakocsis06@gmail.com>
2016-06-29 11:10:28 +0200

    person class done

--- person.py
+++ person.py
@@ -1,2 +1,21 @@
 class Person:
-    pass
+    FEELINGS = ["neutral", "satisfied", "happy", "sad", "proud", "nervous", "mad", "energized", "uncomfortable"]
+    ENERGY_LEVELS = [x for x in range(1, 11)]
+
+    def __init__(self, first_name, last_name, year_of_birth, gender):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.year_of_birth = year_of_birth
+        self.gender = gender
+        self.energy_level = 1
+        self.feeling = "neutral"
+
+    def drink_coffee(self):
+        self.feeling = "energized"
+        self.energy_level += 2
+        return self
+
+    def lunch_break(self):
+        self.energy_level += 2
+        # + codecool_class attention!!
+        return self
