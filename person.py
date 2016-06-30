4b5c611bb5ccc3b4d5f73cfb9783102585173ba1
DorciM <dora.medgyasszay@gmail.com>
2016-06-30 10:27:56 +0200

    inheritance fixed

--- person.py
+++ person.py
@@ -1,14 +1,15 @@
-class Person:
+class Person(object):
     FEELINGS = ["neutral", "satisfied", "happy", "sad", "proud", "nervous", "mad", "energized", "uncomfortable"]
     ENERGY_LEVELS = [x for x in range(1, 11)]
 
-    def __init__(self, first_name, last_name, year_of_birth, gender):
+    def __init__(self, first_name, last_name, year_of_birth, gender, *args, **kwargs):
         self.first_name = first_name
         self.last_name = last_name
         self.year_of_birth = year_of_birth
         self.gender = gender
         self.energy_level = 1
         self.feeling = "neutral"
+        super().__init__(*args, **kwargs)
 
     def drink_coffee(self):
         self.feeling = "energized"
