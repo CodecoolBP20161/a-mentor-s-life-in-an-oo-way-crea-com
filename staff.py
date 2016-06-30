8fd4d65d92d51b8dfcdc287daea13d78687b06cb
cickib <bellakocsis06@gmail.com>
2016-06-30 14:39:51 +0200

    set_school added to staff, creepy guy prints

--- staff.py
+++ staff.py
@@ -2,17 +2,23 @@ from person import Person
 
 
 class Staff():
-    def __init__(self, *args, **kwargs):
-        super(Staff, self).__init__(*args, **kwargs)
+    def __init__(self):
+        pass
+
+    def set_school(self, school):
+        self.school = school
 
     def checking_progress(self):
         self.feeling = "proud"
+        print("{} checked progress and now feeling {}.".format(self.first_name, self.feeling))
         return self
 
     def feedback(self):
         self.feeling = "happy"
+        print("{} gave feedback and now feeling {}.".format(self.first_name, self.feeling))
         if hasattr(self, "creepiness_level"):
             self.creepiness_level = False
+            print("{}'s creepiness is reduced.".format(self.first_name))
         else:
             pass
         return self
